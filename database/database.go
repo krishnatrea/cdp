586372c64259396e070df039dad3d1a03f0eb25e
Harshit Sharma <krishnatrea@gmail.com>
2025-01-02 22:51:46 +0530

    feat: added postgress and slog

--- database/database.go
+++ database/database.go
@@ -3,37 +3,33 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log/slog"
 	"os"
 	"strconv"
 
-	_ "github.com/lib/pq" // don't forget to add it. It doesn't be added automatically
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
-var Db *sql.DB //created outside to make it global.
+var Db *sql.DB
 
-// make sure your function start with uppercase to call outside of the directory.
 func ConnectDatabase() {
-
-	// err := godotenv.Load() //by default, it is .env so we don't have to write
-	// if err != nil {
-	// 	fmt.Println("Error is occurred  on .env file please check")
-	// }
-	//we read our .env file
-	host := os.Getenv("HOST")
-	port, _ := strconv.Atoi(os.Getenv("PORT")) // don't forget to convert int since port is int type.
-	user := os.Getenv("USER")
-	dbname := os.Getenv("DB_NAME")
-	pass := os.Getenv("PASSWORD")
-
-	// set up postgres sql to open it.
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Error is occurred  on .env file please check")
+	}
+	host := os.Getenv("DB_HOST")
+	port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+	user := os.Getenv("POSTGRES_USER")
+	dbname := os.Getenv("POSTGRES_DB")
+	pass := os.Getenv("POSTGRES_PASSWORD")
 	psqlSetup := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
 		host, port, user, dbname, pass)
 	db, errSql := sql.Open("postgres", psqlSetup)
 	if errSql != nil {
-		fmt.Println("There is an error while connecting to the database ", errSql)
-		panic(errSql)
+		slog.Error("There is an error while connecting to the database", "Error", errSql)
 	} else {
 		Db = db
-		fmt.Println("Successfully connected to database!")
+		slog.Info("Successfully connected to database!")
 	}
 }
