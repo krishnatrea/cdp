36468a2dcb67145b975eccb4fb2b842e56b815a8
Harshit Sharma <krishnatrea@gmail.com>
2025-01-14 22:00:48 +0530

    added jwt token

--- internal/tokenutil/tokenutil.go
+++ internal/tokenutil/tokenutil.go
@@ -1,2 +1,30 @@
 package tokenutil
 
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/krishnatrea/cdp/database/model"
+	"github.com/krishnatrea/cdp/domain"
+)
+
+func CreateAccessToken(user model.User, secret string, expire int) (string, error) {
+	exp := time.Now().Add(time.Hour * time.Duration(expire)).Unix()
+	clames := &domain.JwtCustomClaims{
+		Name: user.Firstname,
+		ID:   user.ID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: exp,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, clames)
+
+	t, err := token.SignedString([]byte(secret))
+
+	if err != nil {
+		return "", err
+	}
+
+	return t, err
+}
